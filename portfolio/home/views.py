03402e24eaf0e9d0dc7ff70bf1ddd6108802d921
Gareth Moger <Gareth Moger>
2017-04-02 20:30:25 +0100

    Page titles

--- portfolio/home/views.py
+++ portfolio/home/views.py
@@ -8,25 +8,25 @@ from .forms import ContactForm
 
 
 def home(request):
-    title_text = 'Home'
+    title_text = 'Home - GarethMoger.com'
     post_list = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')[0:2]
     projects = Projects.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')[0:3]
     return render(request, 'home/home.html', {'posts': post_list, 'title': title_text, 'projects': projects})
 
 
 def about(request):
-    title_text = 'About'
+    title_text = 'About - GarethMoger.com'
     return render(request, 'home/about.html', {'title': title_text})
 
 
 def projects(request):
-    title_text = 'Projects'
+    title_text = 'Projects - GarethMoger.com'
     projects = Projects.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'home/projects.html', {'title': title_text, 'projects': projects})
 
 
 def contact(request):
-    title_text = 'Contact'
+    title_text = 'Contact - GarethMoger.com'
     if request.method == 'GET':
         form = ContactForm()
     else:
@@ -50,5 +50,5 @@ def contact(request):
 
 
 def thanks(request):
-    title_text = 'Thank you!'
+    title_text = 'Thank you! - GarethMoger.com'
     return render(request, 'home/thanks.html', {'title': title_text})
