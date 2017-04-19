b830b411b585b72b1fa01c012f2edde10e6ff4ad
Gareth Moger <garethmoger@gmail.com>
2017-04-19 15:44:50 +0100

    Blog views consolidated - removed tags/category views

--- portfolio/blog/urls.py
+++ portfolio/blog/urls.py
@@ -5,7 +5,7 @@ app_name = 'blog'
 
 urlpatterns = [
     url(r'^$', views.posts, name='posts'),
+    url(r'^category/(?P<cat>[-\w]+)/$', views.posts, name='posts'),
+    url(r'^tag/(?P<tag>[-\w]+)/$', views.posts, name='posts'),
     url(r'^post/(?P<pk>\d+)/(?P<slug>[\w-]+)$', views.post_detail, name='post_detail'),
-    url(r'^category/(?P<name>[-\w]+)/$', views.category, name='category'),
-    url(r'^tag/(?P<name>[-\w]+)/$', views.tag, name='tag'),
 ]
--- portfolio/blog/views.py
+++ portfolio/blog/views.py
@@ -5,97 +5,27 @@ from django.db.models import Count
 from django.utils import timezone
 
 
-def tag(request, name):
-    title_text = name + ' - GarethMoger.com'
-    description = 'My latest blog posts to share knowledge and ideas in data analysis and web development'
-    tagPK = Tag.objects.only('pk').get(slug=name).id
-    post_list = Post.objects.filter(tags=tagPK).order_by('-published_date')
-    latest_list = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')[0:4]
-    cat_count = Category.objects.all().annotate(catcount=Count('post'))
-    tag_count = Tag.objects.all().annotate(tagcount=Count('post'))
-    post_count = Post.objects.count()
-
-    paginator = Paginator(post_list, 5)
-
-    try:
-        page = int(request.GET.get('page', '1'))
-    except:
-        page = 1
-
-    try:
-        posts = paginator.page(page)
-    except PageNotAnInteger:
-        posts = paginator.page(1)
-    except EmptyPage:
-        posts = paginator.page(paginator.num_pages)
-
-    index = posts.number - 1
-    max_index = len(paginator.page_range)
-    start_index = index - 7 if index >= 7 else 0
-    end_index = index + 7 if index <= max_index - 7 else max_index
-    page_range = paginator.page_range[start_index:end_index]
-
-    return render(request, 'blog/posts.html', {
-        'posts': posts,
-        'title': title_text,
-        'latest': latest_list,
-        'tag_count': tag_count,
-        'categories': cat_count,
-        'page_range': page_range,
-        'post_count': post_count,
-        'description': description
-    })
-
-def category(request, name):
-    title_text = name + ' - GarethMoger.com'
-    description = 'My latest blog posts to share knowledge and ideas in data analysis and web development'
-    catPK = Category.objects.only('pk').get(name=name).id
-    post_list = Post.objects.filter(category=catPK).order_by('-published_date')
-    latest_list = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')[0:4]
-    cat_count = Category.objects.all().annotate(catcount=Count('post'))
-    tag_count = Tag.objects.all().annotate(tagcount=Count('post'))
-    post_count = Post.objects.count()
-
-    paginator = Paginator(post_list, 5)
-
-    try:
-        page = int(request.GET.get('page', '1'))
-    except:
-        page = 1
-
-    try:
-        posts = paginator.page(page)
-    except PageNotAnInteger:
-        posts = paginator.page(1)
-    except EmptyPage:
-        posts = paginator.page(paginator.num_pages)
-
-    index = posts.number - 1
-    max_index = len(paginator.page_range)
-    start_index = index - 7 if index >= 7 else 0
-    end_index = index + 7 if index <= max_index - 7 else max_index
-    page_range = paginator.page_range[start_index:end_index]
-
-    return render(request, 'blog/posts.html', {
-        'posts': posts,
-        'title': title_text,
-        'latest': latest_list,
-        'tag_count': tag_count,
-        'categories': cat_count,
-        'page_range': page_range,
-        'post_count': post_count,
-        'description': description
-    })
-
-def posts(request):
+def posts(request, cat='', tag=''):
     title_text = 'Blog - GarethMoger.com'
     description = 'My latest blog posts to share knowledge and ideas in data analysis and web development'
-    post_list = Post.objects.all().order_by('-published_date')
+
+    #Generate post list according to URL
+    if cat:
+        catPK = Category.objects.get(name=cat).id
+        post_list = Post.objects.filter(category=catPK).order_by('-published_date')
+    elif tag:
+        tagPK = Tag.objects.get(slug=tag).id
+        post_list = Post.objects.filter(tags=tagPK).order_by('-published_date')
+    else:
+        post_list = Post.objects.all().order_by('-published_date')
+
+    #Data for side list groups
     latest_list = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')[0:4]
     cat_count = Category.objects.all().annotate(catcount=Count('post'))
     tag_count = Tag.objects.all().annotate(tagcount=Count('post'))
     post_count = Post.objects.count()
 
+    #Pagination
     paginator = Paginator(post_list, 5)
 
     try:
@@ -130,6 +60,7 @@ def posts(request):
 def post_detail(request, pk, slug):
     title_text = Post.objects.only('pk').get(pk=pk).title
     description = Post.objects.only('pk').get(pk=pk).description
+
     latest_list = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')[0:4]
     post = get_object_or_404(Post, pk=pk)
     tags = Tag.objects.all()
