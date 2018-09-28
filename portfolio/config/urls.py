bc0105a78db687b64bd054bcee72a1ff9ba1555f
gmoger-jisc <gareth.moger@jisc.ac.uk>
2018-09-28 13:11:54 +0100

    content removed, only homepage accessable

--- portfolio/config/urls.py
+++ portfolio/config/urls.py
@@ -12,14 +12,13 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'', include('home.urls')),
     url(r'^gmadmin/', admin.site.urls),
-    url(r'^blog/', include('blog.urls')),
+    url(r'^blog/', include('home.urls')),
     url(r'^about/', include('home.urls')),
-    url(r'^projects/', include('projects.urls')),
+    url(r'^projects/', include('home.urls')),
     url(r'^contact/', include('home.urls')),
     url(r'^thanks/', include('home.urls')),
     url(r'^search/', include('home.urls')),
     url(r'^ckeditor/', include('ckeditor_uploader.urls')),
-	
 ]
 
 # serving media files only on debug mode
@@ -27,3 +26,17 @@ if settings.DEBUG:
     urlpatterns += [
         url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 	] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+# origin patterns before removal
+# urlpatterns = [
+#     url(r'', include('home.urls')),
+#     url(r'^gmadmin/', admin.site.urls),
+#     url(r'^blog/', include('blog.urls')),
+#     url(r'^about/', include('home.urls')),
+#     url(r'^projects/', include('projects.urls')),
+#     url(r'^contact/', include('home.urls')),
+#     url(r'^thanks/', include('home.urls')),
+#     url(r'^search/', include('home.urls')),
+#     url(r'^ckeditor/', include('ckeditor_uploader.urls')),
+# ]
--- portfolio/home/urls.py
+++ portfolio/home/urls.py
@@ -5,8 +5,18 @@ app_name = 'home'
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
-    url(r'^about/$', views.about, name='about'),
-    url(r'^contact/$', views.contact, name='contact'),
-    url(r'^thanks/$', views.thanks, name='thanks'),
-    url(r'^search/$', views.search, name='search'),
+    url(r'^about/$', views.home),
+    url(r'^contact/$', views.home),
+    url(r'^thanks/$', views.home),
+    url(r'^search/$', views.home),
+    url(r'^blog/$', views.home),
+    url(r'^projects/$', views.home)
 ]
+
+# urlpatterns = [
+#     url(r'^$', views.home, name='home'),
+#     url(r'^about/$', views.about, name='about'),
+#     url(r'^contact/$', views.contact, name='contact'),
+#     url(r'^thanks/$', views.thanks, name='thanks'),
+#     url(r'^search/$', views.search, name='search'),
+# ]
